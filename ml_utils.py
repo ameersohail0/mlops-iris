d32f91b8aa554099d0c9d74f31f780585b1c6706
Ameer Sohail <ameersohail0@gmail.com>
2021-07-10 18:10:45 +0530

    [TASK3] Added Logistic regression classifier as alternative

--- ml_utils.py
+++ ml_utils.py
@@ -2,9 +2,17 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
 
 # define a Gaussain NB classifier
-clf = GaussianNB()
+gnb_clf = GaussianNB()
+
+# creating an alternative classifier
+# using a logistic regression classifier
+logreg_clf = LogisticRegression(penalty='l2',C=1.0, max_iter=10000)
+
+# classifier with best accuracy will be used
+clf = None
 
 # define the class encodings and reverse encodings
 classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
@@ -17,11 +25,28 @@ def load_model():
 
     # do the test-train split and train the model
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    clf.fit(X_train, y_train)
 
-    # calculate the print the accuracy score
-    acc = accuracy_score(y_test, clf.predict(X_test))
-    print(f"Model trained with accuracy: {round(acc, 3)}")
+    # Training Guassain NB Classifier
+    gnb_clf.fit(X_train, y_train)
+
+    # Training Logistic Regression Classifier
+    logreg_clf.fit(X_train, y_train)
+
+    # calculate the accuracy score of Guassain NB Classifier
+    gnb_acc = accuracy_score(y_test, gnb_clf.predict(X_test))
+
+    # calculate the accuracy score of Logistic Regression Classifier
+    logreg_acc = accuracy_score(y_test, logreg_clf.predict(X_test))
+
+    # modifying the global classifier
+    global clf
+    # Using the model with best accuracy
+    if (logreg_acc >= gnb_acc):
+        clf = logreg_clf
+        print(f"Logistic Regression Model trained with accuracy: {round(logreg_acc, 3)}")
+    else :
+        clf = gnb_clf
+        print(f"Guassain NB Model trained with accuracy: {round(gnb_acc, 3)}")
 
 
 # function to predict the flower using the model
